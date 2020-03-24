01fd92340c6057b97bd17342045c82dec98b2880
geraldinejuery <juerygeraldine26@gmail.com>
2020-03-24 21:00:04 +0100

    fix #2 requete P1-5.1 sur activity

---
+++ P1-5-1-select-all-activities.sql
@@ -0,0 +1,6 @@
+SELECT title, description, username
+FROM public.user
+LEFT OUTER JOIN public.activity
+ON public.user.id = public.activity.owner_id
+WHERE creation_date > '2019-09-01'
+ORDER BY title, username
