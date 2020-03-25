483276c6c75122244f37c7e697e13a6088c952c0
geraldinejuery <juerygeraldine26@gmail.com>
2020-03-25 13:47:25 +0100

    fix #2 requete P1-5.1 sur activity

--- P1-4-1-select-old-users.sql
+++ P1-4-1-select-old-users.sql
@@ -1,4 +1,4 @@
 SELECT username, date_created
 FROM public.user
 WHERE date_created < '2019-09-01'
-ORDER BY username
+ORDER BY username ASC
--- P1-5-1-select-all-activities.sql
+++ P1-5-1-select-all-activities.sql
@@ -3,4 +3,4 @@ FROM public.user
 LEFT OUTER JOIN public.activity
 ON public.user.id = public.activity.owner_id
 WHERE creation_date > '2019-09-01'
-ORDER BY title, username
+ORDER BY title, username ASC
